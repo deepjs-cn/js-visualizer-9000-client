e3aa78e0800f07b0c69ea9c18d7656d775318d21
cloudyan <cloudyan@users.noreply.github.com>
2023-06-16 13:43:16 +0000

    deploy: 2f434debf8b13286894ba3768be5859ac5be0cdf

---
+++ precache-manifest.bd0c1dcc03244d02eb270c57554f4a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ded5357123a3ec244fa7",
+    "url": "/static/css/main.5fbc3456.chunk.css"
+  },
+  {
+    "revision": "ded5357123a3ec244fa7",
+    "url": "/static/js/main.b99ad4b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0da7c5a770ee62185047",
+    "url": "/static/js/2.7f47a97c.chunk.js"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/js-logo.0cded3a3.png"
+  },
+  {
+    "revision": "f81d9a596f2f18756b282dc3c1bef8f6",
+    "url": "/index.html"
+  }
+];
